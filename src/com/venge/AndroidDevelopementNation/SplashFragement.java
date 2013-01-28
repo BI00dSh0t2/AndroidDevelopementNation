02d18d6a1128d42ce4dd776f05fe4e870bd73572
Joshua Dixon <joshua.dixon@live.com>
2013-01-28 16:07:33 -0500

    Initial Commit

---
+++ src/com/venge/AndroidDevelopementNation/SplashFragement.java
@@ -0,0 +1,17 @@
+package com.venge.AndroidDevelopementNation;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class SplashFragement extends FragmentActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.splash);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/venge/AndroidDevelopementNation/about.java
@@ -0,0 +1,16 @@
+package com.venge.AndroidDevelopementNation;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class about extends Fragment {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
